f92bc34f2e4e6e6269df289e2f3bf915a89706e8
nurdoolot.tagaev <nurdoolot.tagaev@iaau.edu.kg>
2018-02-18 00:12:10 +0600

    Update convert.sh
    modified convert.sh which can now convert gdoc files to rst files too.

--- convert.sh
+++ convert.sh
@@ -2,8 +2,11 @@
 
 for gdoc in `find . -type f -regex '.*\.docx'`; do
   mdfile="$(echo $gdoc | sed -e 's/\.docx$/\.md/')"
+  rstfile="$(echo $gdoc | sed -e 's/\.docx$/\.rst/')"
   echo "Converting $gdoc to markdown ..."
   w2m "$gdoc" > "$mdfile"
+  echo "Converting $gdoc to reStructuredText ..."
+  w2m "$gdoc" > "$rstfile"
 
   if [ -z "$(head -n 1 $mdfile | egrep '^# ')" ]; then
     basefile="$(basename $mdfile)"
@@ -13,8 +16,22 @@ for gdoc in `find . -type f -regex '.*\.docx'`; do
     rm "$mdfile"
     mv "$mdfile.new" "$mdfile"
   fi
+  if [ -z "$(head -n 1 $rstfile | egrep '^# ')" ]; then
+    basefile="$(basename $rstfile)"
+    title='# '"$(echo $basefile | sed -e 's/\.md//' -e 's/_/ /g')"
+    echo "$title"  > "$rstfile.new"
+    cat "$rstfile" >> "$rstfile.new"
+    rm "$rstfile"
+    mv "$rstfile.new" "$rstfile"
+  fi
 
   cat >> "$mdfile" <<-EOF
+<!--
+ORIGIN: gdocs
+DATE: $(date)
+-->
+EOF
+  cat >> "$rstfile" <<-EOF
 <!--
 ORIGIN: gdocs
 DATE: $(date)
