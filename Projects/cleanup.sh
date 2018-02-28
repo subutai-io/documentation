2668620fcf92ce297d620598efaca276f2c3fb35
Alex O. Karasulu <akarasulu@optimal-dynamics.com>
2018-02-28 10:00:35 +0600

    adding some code to refresh after changes to existing files

--- Projects/cleanup.sh
+++ Projects/cleanup.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 
-PROJECTS_DIR=/readthedocs/Projects
+for proj_dir in `find /readthedocs/Projects -type d`; do
+  if [ "$proj_dir" == "/readthedocs/Projects" ]; then
+    continue;
+  fi
+
+  proj_name="$(basename $proj_dir)"
+  echo [DEBUG] proj_name = $proj_name
+  cd $proj_dir
+  git checkout .
+  cd ..
+done
 
 # TODO check if it's committed, and if so just checkout removing edits
 # TODO delete only ones that are generated and not committed 
 # find $PROJECTS_DIR -type f -regex '.*\.rst' | xargs rm
+
