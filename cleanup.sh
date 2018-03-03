ae084431ed2cfa30cdce9772bd828cc275fded5c
Alex O. Karasulu <akarasulu@optimal-dynamics.com>
2018-03-04 05:04:34 +0600

    nothing

--- cleanup.sh
+++ cleanup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+rm index.rst
+
 find . -type f -regex '.*\.docx' | xargs rm
 for md in `find . -type f -regex '.*\.md'`; do
   if [ -n "$(tail -n 3 $md | grep ORIGIN | grep gdocs)" ]; then
