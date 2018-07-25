5008dc7d5c23cd49e8db8148a327e5452c77b0ec
Felipe Fonseca <felipefonseca@gmail.com>
2018-07-25 19:05:10 -0300

    removing useless files

--- docs-subutai/cache/compiled/files/aa30c7578b50bdca686b6c3825bc8fbc.yaml.php
+++
@@ -1,9 +0,0 @@
-<?php
-return [
-    '@class' => 'Grav\\Common\\File\\CompiledYamlFile',
-    'filename' => '/home/felipe/public_html/readthedocs/docs-subutai/user/plugins/customadmin/customadmin.yaml',
-    'modified' => 1532554913,
-    'data' => [
-        'enabled' => true
-    ]
-];
--- docs-subutai/cache/compiled/files/ee3e03504983d46bc8621853be742ec4.yaml.php
+++
@@ -1,9 +0,0 @@
-<?php
-return [
-    '@class' => 'Grav\\Common\\File\\CompiledYamlFile',
-    'filename' => '/home/felipe/public_html/readthedocs/docs-subutai/user/config/plugins/highlight.yaml',
-    'modified' => 1532554913,
-    'data' => [
-        'theme' => 'learn'
-    ]
-];
